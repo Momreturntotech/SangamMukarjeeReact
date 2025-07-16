ab39b0085b8940d716befa131da3acf649058377
Madhura BS <madhurabs@Madhuras-MacBook-Air.local>
2025-07-16 16:52:01 +0530

    React Class Based life Cycle Methods

--- src/components/class-based-component.jsx
+++ src/components/class-based-component.jsx
@@ -5,6 +5,8 @@ class ClassBasedComponent extends Component{
     state={
         showText:false,
         changeColor:false,
+        count:0,
+        countColor:"green"
   };
 //function to handle Click
   handleClick=()=>{
@@ -27,13 +29,38 @@ class ClassBasedComponent extends Component{
 
 */
 
+componentDidMount(){// when ever you want to something on page load you are going to do under it 
+    console.log("This is Component Did mount");
+   this.setState({
+        showText:!this.state.showText,
+        changeColor:!this.state.changeColor,
+    })
+    
+}
+
+componentDidUpdate(prevProps,prevState){//when ever the state value is updated 
+    console.log("This is Component Did Update the State value is Updated ");
+    console.log(prevProps,prevState)
+     console.log(prevProps,this.state)
+      if (prevState.count !== this.state.count && this.state.count === 10) {// review this code
+      this.setState({ countColor: "purple" });
+    }
+
+}
+
+componentWillUnmount(){
+    console.log("Component is getting unmounted ")
+}
+
     render(){
-        console.log(this.state);
+        //console.log(this.state);
         return (
             <>
             {this.state.showText?<h3 > This is Class Based Component</h3>:null}
+            <h4 style={{color:this.state.countColor}}> This is count varibale {this.state.count}</h4>
             
             <button onClick={()=>this.handleClick()} style={{color:this.state.changeColor?'green':'red'}}>Toggle The Text</button>
+            <button onClick={()=> this.setState({count:this.state.count+1})}>Increase Count</button>
             </>
         )
     }
