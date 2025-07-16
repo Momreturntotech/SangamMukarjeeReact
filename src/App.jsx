049fdd3b1e4a0005a2bbeaa110cf8dcffb8e4f46
Madhura BS <madhurabs@Madhuras-MacBook-Air.local>
2025-07-16 16:01:15 +0530

    state and setState in class based component

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,14 @@ function App() {
 
   return (
     <>
-    <h1>This is App .jsx</h1>
- {/* <Products/> */} 
- <ConditionalRender/>
+    <ClassBasedComponent/>
+ {/* <Products/> 
+ 
+  <ConditionalRender/>
+ 
+ 
+ */} 
+
     </>
   )
 }
--- src/components/class-based-component.jsx
+++ src/components/class-based-component.jsx
@@ -1,9 +1,40 @@
 import { Component } from "react";
 
 class ClassBasedComponent extends Component{
+//1st method to create state in class based component
+    state={
+        showText:false,
+        changeColor:false,
+  };
+//function to handle Click
+  handleClick=()=>{
+    console.log("button Clicked");
+    //this is not recommended
+    //this.state.showText=!this.state.showText}
+    this.setState({
+        showText:!this.state.showText,
+        changeColor:!this.state.changeColor,
+    })
+}
+/* 
+//2nd method to create a state in class based component 
+  constructor(props){
+    super(props)
+    this.state={
+        showText:false,
+    }
+  }
+
+*/
+
     render(){
+        console.log(this.state);
         return (
-            <h1>This is Class Based Component</h1>
+            <>
+            {this.state.showText?<h3 > This is Class Based Component</h3>:null}
+            
+            <button onClick={()=>this.handleClick()} style={{color:this.state.changeColor?'green':'red'}}>Toggle The Text</button>
+            </>
         )
     }
 }
