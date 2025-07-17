69a0ab393c0bf6269eff7208b0dac285f9d2bfc3
Madhura BS <madhurabs@Madhuras-MacBook-Air.local>
2025-07-17 11:11:43 +0530

    managed the loading state

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,13 +3,19 @@ import { useEffect, useState } from "react";
 function Users(){
 
     const [userData,setUserData]=useState([]);
+    const [loading,setLoading]=useState(false)
 
     const fetchData=async()=>{
         try {
+            setLoading(true)
             const apiResponse= await fetch('https://dummyjson.com/users',{method:'GET'})
             const data = await apiResponse.json();
             if(data?.users){
                  setUserData(data.users)
+                 setLoading(false)
+            }
+            else{
+                setLoading(false)
             }
            
             console.log(userData)
@@ -26,6 +32,9 @@ function Users(){
     
     return(
         <>
+        <div>
+            {loading?<h1>The Data is Loading</h1>:null}
+        </div>
         <div className="">
             {
             (userData && userData.length >0)?
